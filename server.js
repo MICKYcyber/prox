27170efcbeef3078cf3141dad1ff7cdfe050a2c5
MICKYcyber <goshdarnmicky@gmail.com>
2025-09-05 19:26:31 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/proxy', async (req, res) => {
+  const targetUrl = req.query.url;
+  if (!targetUrl) {
+    return res.status(400).send('URL parameter is required');
+  }
+
+  try {
+    const response = await axios.get(targetUrl, { responseType: 'text' });
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching the requested URL');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server running on port ${port}`);
+});
