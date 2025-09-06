06d53e3c7fae1aa5d6960c6422e8956587d368ce
MICKYcyber <goshdarnmicky@gmail.com>
2025-09-06 15:59:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fetch = require("node-fetch");
 const { JSDOM } = require("jsdom");
 const app = express();
 
+// Default site to mirror
 const TARGET_URL = "https://sites.google.com/view/22y/";
 
 function absolutify(url, base) {
@@ -13,26 +14,36 @@ function absolutify(url, base) {
   }
 }
 
-app.get("/mirror", async (req, res) => {
+// Core mirror handler
+async function handleMirror(url, res) {
   try {
-    const response = await fetch(TARGET_URL);
-    let html = await response.text();
+    const response = await fetch(url);
+    let contentType = response.headers.get("content-type") || "";
+    res.set("Access-Control-Allow-Origin", "*");
 
+    // If it's not HTML, just pipe it back
+    if (!contentType.includes("text/html")) {
+      const buffer = await response.buffer();
+      res.set("content-type", contentType);
+      return res.send(buffer);
+    }
+
+    // Parse HTML and rewrite asset URLs
+    let html = await response.text();
     const dom = new JSDOM(html);
     const document = dom.window.document;
 
-    // Rewriting function
     const rewriteAttr = (selector, attr) => {
       document.querySelectorAll(selector).forEach(el => {
         const val = el.getAttribute(attr);
         if (val) {
-          const abs = absolutify(val, TARGET_URL);
+          const abs = absolutify(val, url);
           el.setAttribute(attr, `/mirror?url=${encodeURIComponent(abs)}`);
         }
       });
     };
 
-    // Rewrite all important tags
+    // Rewrite links/scripts/images/etc
     rewriteAttr("a[href]", "href");
     rewriteAttr("link[href]", "href");
     rewriteAttr("script[src]", "src");
@@ -40,29 +51,18 @@ app.get("/mirror", async (req, res) => {
     rewriteAttr("iframe[src]", "src");
     rewriteAttr("form[action]", "action");
 
-    html = dom.serialize();
-
-    res.set("Access-Control-Allow-Origin", "*");
-    res.send(html);
+    res.send(dom.serialize());
   } catch (err) {
     res.status(500).send("Proxy error: " + err.message);
   }
-});
+}
 
-// Handle /mirror?url=...
+// Default mirror endpoint
 app.get("/mirror", async (req, res) => {
-  const url = req.query.url;
-  if (!url) return res.status(400).send("Missing url");
-
-  try {
-    const response = await fetch(url);
-    res.set("Access-Control-Allow-Origin", "*");
-    res.send(await response.text());
-  } catch (err) {
-    res.status(500).send("Proxy error: " + err.message);
-  }
+  const target = req.query.url || TARGET_URL;
+  await handleMirror(target, res);
 });
 
 app.listen(3000, () => {
-  console.log("Proxy running at http://localhost:3000");
+  console.log("Proxy running at http://localhost:3000/mirror");
 });
